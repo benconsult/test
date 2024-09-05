0ae1897979abb37ec86b66af68931261ccdca713
unknown <bendayo38@gmail.com>
2024-09-05 16:33:53 +0100

    added Dcokerfile and nginx config

---
+++ robofriends-master/README.md
@@ -0,0 +1,5 @@
+# robofriends
+
+# It uses a fetch Api
+
+#A simple GUI that fetches the contacts of friends and displays them on the web browser. 
---
+++ robofriends-master/src/robots.js
@@ -0,0 +1,71 @@
+
+export const robots = [
+{
+    id:1,
+    name:'Benjy Aremu',
+    username:'benny',
+    email:'benjy@gmail.com'
+},
+{
+    id:2,
+    name:'Bola Alex',
+    username:'bollybabe',
+    email:'bolly@gmail.com'
+},
+{
+    id:3,
+    name:'James Max',
+    username:'jjrollynx',
+    email:'james123@gmail.net'
+},
+{
+    id:4,
+    name:'Ervin Howell',
+    username:'howwy',
+    email:'sincers@april.tiv'
+},
+{
+    id:5,
+    name:'Clinton Beach',
+    username:'Antonette',
+    email:'clinty@beach.biz'
+},
+{
+    id:6,
+    name:'Ire Williams',
+    username:'willyboy',
+    email:'saphirretech@soft.com'
+},
+{
+    id:7,
+    name:'Tolu Ade',
+    username:'tolex',
+    email:'toluadex@gmail.com'
+},
+{
+    id:8,
+    name:'Blessing Damwal',
+    username:'blessoey',
+    email:'beessygirl@gmail.com'
+},
+{
+    id:9,
+    name:'David Mark',
+    username:'davido',
+    email:'davido@gmail.com'
+},
+{
+    id:10,
+    name:'Kingsley Moore',
+    username:'mrking',
+    email:'solexy@yahoo.com'
+}
+
+
+
+
+
+
+
+
+]
